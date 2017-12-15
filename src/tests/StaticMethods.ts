3c83aa40c44cf384a60cd62882a457b0609a0843
Silvestr Baibuz <silvestr-b@yandex.ru>
2017-12-15 05:40:34 +0300

    refactoring of .all method tests

--- src/tests/StaticMethods.ts
+++ src/tests/StaticMethods.ts
@@ -6,67 +6,65 @@ import { isSyncPromise, isRealPromise } from './utils'
 describe('StaticMethods', () => {
    describe('.all', () => {
 
-      it('argument is value', (done) => {
-         const promise = SyncPromise.all([5])
-            .then(result => {
-               expect(result).to.be.eql([5])
-            })
-         expect(isSyncPromise(promise)).to.be.true
-         promise.then(() => done())
-      })
-
-      it('argument is resolved SyncPromise', (done) => {
-         const promise = SyncPromise.all([SyncPromise.resolve(5)])
-            .then(result => {
-               expect(result).to.be.eql([5])
-            })
-         expect(isSyncPromise(promise)).to.be.true
-         promise.then(() => done())
-      })
-
-      it('argument is rejected SyncPromise', (done) => {
-         const promise = SyncPromise.all([SyncPromise.reject(5)])
-            .then(null, err => {
-               expect(err).to.be.eql(5)
-            })
-         expect(isSyncPromise(promise)).to.be.true
-         promise.then(() => done())
-      })
-
-      it('argument is resolved Promise', (done) => {
-         const promise = SyncPromise.all([Promise.resolve(5)])
-            .then(result => {
-               expect(result).to.be.eql([5])
-               done()
-            })
-         expect(isRealPromise(promise)).to.be.true
-      })
-
-      it('argument is rejected Promise', (done) => {
-         const promise = SyncPromise.all([Promise.reject(5)])
-            .then(null, err => {
-               expect(err).to.be.eql(5)
-               done()
-            })
-         expect(isRealPromise(promise)).to.be.true
-      })
-
-      it('random type arguments without Promise', (done) => {
+      it('When passed array do not has Promise, should return SyncPromise', done => {
          const promise = SyncPromise.all(['FakeString', null, 10, SyncPromise.resolve(5)])
-            .then(result => {
-               expect(result).to.be.eql(['FakeString', null, 10, 5])
-            })
          expect(isSyncPromise(promise)).to.be.true
          promise.then(() => done())
       })
 
-      it('random type arguments with Promise', (done) => {
-         const promise = SyncPromise.all(['FakeString', null, 10, Promise.resolve(5)])
-            .then(result => {
-               expect(result).to.be.eql(['FakeString', null, 10, 5])
-               done()
-            })
+      it('When passed array has Promise, should return Promise', done => {
+         const promise = SyncPromise.all(['FakeString', null, 10, SyncPromise.resolve(5), Promise.resolve(10)])
          expect(isRealPromise(promise)).to.be.true
+         promise.then(() => done())
+      })
+
+      it('When passed array with value, should be resolved with array which has that value', done => {
+         SyncPromise.all([5])
+            .then(result => {
+               expect(result).to.be.eql([5])
+            })
+            .then(done)
+      })
+
+      it('When passed array with resolved SyncPromise, should be resolved with array which has value of that SyncPromise', done => {
+         SyncPromise.all([1, SyncPromise.resolve(5), 10])
+            .then(result => {
+               expect(result).to.be.eql([1, 5, 10])
+            })
+            .then(done)
+      })
+
+      it('When passed array with rejected SyncPromise, should be rejected with value of that SyncPromise', done => {
+         SyncPromise.all([1, SyncPromise.reject(5), 10])
+            .then(null, err => {
+               expect(err).to.be.eql(5)
+            })
+            .then(done)
+      })
+
+      it('When passed array with resolved Promise, should be resolved with array which has value of that Promise', done => {
+         SyncPromise.all([1, Promise.resolve(5), 10])
+            .then(result => {
+               expect(result).to.be.eql([1, 5, 10])
+            })
+            .then(done)
+      })
+
+      it('When passed array with rejected Promise, should be rejected with value of that Promise', done => {
+         SyncPromise.all([1, Promise.reject(5), 10])
+            .then(null, err => {
+               expect(err).to.be.eql(5)
+            })
+            .then(done)
+      })
+
+      it('When passed array with many any type items, should be resolved with array which has that values and values of Promises', done => {
+         const fakeObject = {}, fakeArray: any[] = [];
+         SyncPromise.all(['FakeString', null, 10, fakeArray, fakeObject, SyncPromise.resolve(5), Promise.resolve(10)])
+            .then(result => {
+               expect(result).to.be.eql(['FakeString', null, 10, fakeArray, fakeObject, 5, 10])
+            })
+            .then(done)
       })
 
    })
