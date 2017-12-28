4e71db6cad19858f16d836c5e04209aa2dea46aa
Silvestr Baibuz <silvestr-b@yandex.ru>
2017-12-28 20:47:19 +0300

    rewrite to better performance

--- src/SyncPromise.ts
+++ src/SyncPromise.ts
@@ -6,19 +6,31 @@ enum STATES {
    REJECTED = 'REJECTED'
 }
 
+enum RESULT_TYPES {
+   PROMISE = 'PROMISE',
+   SYNCPROMISE = 'SYNCPROMISE',
+   VALUE = 'VALUE'
+}
+
 
 class SyncPromise<T> implements Promise<T> {
 
    readonly [Symbol.toStringTag]: 'Promise';
    private state: STATES = STATES.PENDING;
    private result: T | Promise<T>;
+   private resultType: RESULT_TYPES;
+   private isSyncPromise = true;
 
    constructor(
-      executor: (
+      executor?: (
          resolve: (value: T | Promise<T>) => void,
          reject: (reason: T | Promise<T>) => void
       ) => void
    ) {
+      executor && this.executeExecutor(executor)
+   }
+
+   private executeExecutor(executor: any){
       try {
          executor(this.resolve, this.reject)
       } catch (err) {
@@ -27,77 +39,49 @@ class SyncPromise<T> implements Promise<T> {
    }
 
    then<TResult1 = T, TResult2 = never>(onfulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | undefined | null, onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null): Promise<TResult1 | TResult2> {
-
+      
       if (this.state === STATES.RESOLVED) {
-         if (SyncPromise.isRealPromise(this.result)) {
-            return (<Promise<T>>this.result).then(val => {
-               try {
-                  return (onfulfilled ? onfulfilled(val) : <any>val)
-               } catch (err) {
-                  return Promise.reject(err)
-               }
-            }, err => {
-               try {
-                  return (onrejected ? onrejected(err) : <any>err)
-               } catch (err) {
-                  return Promise.reject(err)
-               }
-            })
+         if(this.resultType === RESULT_TYPES.VALUE){
+            return SyncPromise.resolve(onfulfilled ? this.executeSync(onfulfilled, <T>this.result) : <any>this.result)
          }
 
-         if (SyncPromise.isSyncPromise(this.result)) {
+         if (this.resultType === RESULT_TYPES.SYNCPROMISE) {
             return (<SyncPromise<T>>this.result).then(val => {
-               try {
-                  return (onfulfilled ? onfulfilled(val) : <any>val)
-               } catch (err) {
-                  return SyncPromise.reject(err)
-               }
+               return (onfulfilled ? this.executeSync(onfulfilled, val) : <any>val)
             }, err => {
-               try {
-                  return (onrejected ? onrejected(err) : <any>err)
-               } catch (err) {
-                  return SyncPromise.reject(err)
-               }
+               return (onrejected ? this.executeSync(onrejected, err) : <any>err)
             })
          }
 
-         try {
-            return SyncPromise.resolve(onfulfilled ? onfulfilled(<T>this.result) : <any>this.result)
-         } catch (err) {
-            return SyncPromise.reject(err)
-         }
-
+         if (this.resultType === RESULT_TYPES.PROMISE) {
+            return (<Promise<T>>this.result).then(val => {
+               return (onfulfilled ? this.executeAsync(onfulfilled, val) : <any>val)
+            }, err => {
+               return (onrejected ? this.executeAsync(onrejected, err) : <any>err)
+            })
+         }  
+         
       }
 
       if (this.state === STATES.REJECTED) {
-         if (SyncPromise.isRealPromise(this.result)) {
-            try {
-               return Promise.resolve(onrejected ? onrejected(this.result) : <any>this.result)
-            } catch (err) {
-               return Promise.reject(err)
-            }
-         }
-
-         if (SyncPromise.isSyncPromise(this.result)) {
-            try {
-               return SyncPromise.resolve(onrejected ? onrejected(this.result) : <any>this.result)
-            } catch (err) {
-               return SyncPromise.reject(err)
-            }
-         }
-
-         try {
+         if(this.resultType === RESULT_TYPES.VALUE){
             if (onrejected) {
-               return SyncPromise.resolve(<any>onrejected(<T>this.result))
+               return SyncPromise.resolve(this.executeSync(onrejected, this.result))
             } else {
                return SyncPromise.reject(<any>this.result)
             }
-         } catch (err) {
-            return SyncPromise.reject(err)
          }
 
-      }
+         if (this.resultType === RESULT_TYPES.SYNCPROMISE) {
+            return SyncPromise.resolve(onrejected ? this.executeSync(onrejected, this.result) : <any>this.result)
+         }
 
+         if (this.resultType === RESULT_TYPES.PROMISE) {
+            return Promise.resolve(onrejected ? this.executeAsync(onrejected, this.result) : <any>this.result)
+         }
+        
+      }
+     
       return <any>this
    }
 
@@ -105,19 +89,49 @@ class SyncPromise<T> implements Promise<T> {
       return this.then(null, onrejected)
    }
 
+   private executeAsync(cb: any, value: any){
+      try {
+         return cb(value)
+      } catch (err) {
+         return Promise.reject(err)
+      }
+   }
+
+   private executeSync(cb: any, value: any){
+      try {
+         return cb(value)
+      } catch (err) {
+         return SyncPromise.reject(err)
+      }
+   }
 
    private resolve = (result: T | Promise<T>) => {
-      if (this.state !== STATES.PENDING) return
+      if (this.state !== STATES.PENDING) return this
       this.state = STATES.RESOLVED;
-      this.result = result;
+      this.setResult(result);
+      return this
    }
 
    private reject = (reason: T | Promise<T>) => {
-      if (this.state !== STATES.PENDING) return
+      if (this.state !== STATES.PENDING) return this
       this.state = STATES.REJECTED;
-      this.result = reason;
+      this.setResult(reason);
+      return this
    }
 
+   private setResult(value: any){
+      if(!value || !value.then){
+         this.resultType = RESULT_TYPES.VALUE
+      } else {
+         if(value.isSyncPromise){
+            this.resultType = RESULT_TYPES.SYNCPROMISE
+         } else {
+            this.resultType = RESULT_TYPES.PROMISE
+         }
+      }
+      
+      this.result = value;
+   }
 
    // Variants of overload
    static all<T1, T2, T3, T4, T5>(values: [Promise<T1> | T1, Promise<T2> | T2, Promise<T3> | T3, Promise<T4> | T4, Promise<T5> | T5]): Promise<[T1, T2, T3, T4, T5]>;
@@ -135,12 +149,14 @@ class SyncPromise<T> implements Promise<T> {
       for (let i = 0; i < values.length; i++) {
          const value = values[i];
 
-         if (SyncPromise.isSyncPromise(value) || SyncPromise.isRealPromise(value)) {
-            if (value instanceof SyncPromise) {
-               if (value.state === "RESOLVED") {
+         if (!value || !value.then) {
+            results[i] = value
+         } else {
+            if (value.isSyncPromise) {
+               if (value.state === STATES.RESOLVED) {
                   results[i] = value.result
-               }
-               if (value.state === "REJECTED") {
+               } else
+               if (value.state === STATES.REJECTED) {
                   return SyncPromise.reject(value.result)
                }
             } else {
@@ -152,24 +168,21 @@ class SyncPromise<T> implements Promise<T> {
 
                promises.push(value);
             }
-
-         } else {
-            results[i] = value
          }
       }
 
       if (promises.length > 0) {
          return Promise.all(promises).then(() => results)
       } else {
-         return SyncPromise.resolve(results)
+         return new SyncPromise<any[]>().resolve(results)
       }
    }
 
    static resolve<T>(value: T | Promise<T>) {
-      if (SyncPromise.isSyncPromise(value) || SyncPromise.isRealPromise(value)) {
+      if (value && (<Promise<T>>value).then) {
          return <Promise<T>>value
       }
-      return new SyncPromise<T>((resolve, reject) => resolve(value))
+      return new SyncPromise<T>().resolve(value)
    }
 
    static reject<T>(reason: T | Promise<T>) {
@@ -180,15 +193,15 @@ class SyncPromise<T> implements Promise<T> {
    }
 
    static isPromise(value: any) {
-      return value && value.then && typeof value.then === 'function'
+      return value && value.then
    }
    
    static isRealPromise(value: any) {
-      return value && value.then && typeof value.then === 'function' && !(value instanceof SyncPromise)
+      return value && value.then && !value.isSyncPromise
    }
 
    static isSyncPromise(value: any) {
-      return value instanceof SyncPromise
+      return value && value.isSyncPromise
    }
 
 }
